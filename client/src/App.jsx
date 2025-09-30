c84b12c791fa9fd24a1704c721eafbc1ad33fe75
mannatgupta146 <mannatgupta146@gmail.com>
2025-10-01 01:30:10 +0530

    routing workinh pages compomnets folder added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import SideBar from './components/SideBar'
+import { Route, Routes } from 'react-router-dom'
+import ChatBox from './components/ChatBox'
+import Credits from './pages/Credits'
+import Community from './pages/Community'
 
 const App = () => {
   return (
-    <div>
-      
+    <>
+    <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+      <div className='flex h-screen w-screen'>
+        <SideBar/>
+        <Routes>
+          <Route path="/" element={<ChatBox />} />
+          <Route path="/credits" element={<Credits />} />
+          <Route path="/community" element={<Community />} />
+        </Routes>
+      </div>
     </div>
+    </>
   )
 }
 
