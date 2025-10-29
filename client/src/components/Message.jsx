c565302b94a5eb156d495c391683f2eb3eb5a401
mannatgupta146 <mannatgupta146@gmail.com>
2025-10-30 00:03:07 +0530

    message uppdated

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import moment from 'moment'
 
 const Message = ({message}) => {
   return (
@@ -7,8 +8,8 @@ const Message = ({message}) => {
       {message.role === "user" ? (
         <div className='flex items-start justify-end my-4 gap-2'>
           <div className='flex flex-col gap-2 p-2 px-4 bg-slate-50  dark:bg-[#57317C]/30 border border-[#80609F]/30 rounded-md max-w-2xl'>
-            <p className='text-base dark:text-primary'>{message.content}</p>
-            <span className='text-xs text-gray-400 dark:text-[#B1A6C0]'>{message.timestamp}</span>
+            <p className='text-sm dark:text-primary'>{message.content}</p>
+            <span className='text-xs text-gray-400 dark:text-[#B1A6C0]'>{moment(message.timestamp).fromNow()}</span>
           </div>
           <img src={assets.user_icon} alt="" className='w-8 rounded-full'/>
         </div>
@@ -24,7 +25,7 @@ const Message = ({message}) => {
             <div className='text-sm dark:text-primary reset-tw'>{message.content}</div>
           )
         }
-          <span>{message.timestamp}</span>
+          <span className='text-xs text-gray-400 dark:text-[#B1A6C0] '>{moment(message.timestamp).fromNow()}</span>
         </div>
       )
     }
