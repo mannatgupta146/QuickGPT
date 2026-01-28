37e1002fda6cb4f0a1a34a79fe28df16f97e1f7f
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-28 19:32:29 +0530

    database connected

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
+import connectDB from './configs/db.js'
 
 const app = express()
+
+await connectDB()
 
 // middlewares
 app.use(cors())
@@ -17,5 +20,4 @@ const PORT = process.env.PORT || 3000
 
 app.listen(PORT, ()=> {
     console.log(`server is runnning on ${PORT}`);
-    
 })
