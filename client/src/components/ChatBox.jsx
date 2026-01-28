7ae6b6d5f33990b2df7e35d60556dcbe5a5a3b6c
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-28 16:37:57 +0530

    prompt is working fine

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useAppContext } from '../context/AppContext';
 import { assets } from '../assets/assets';
 import Message from './Message';
 const ChatBox = () => {
 
+  const containerRef = useRef(null)
+
   const {selectedChat, theme} = useAppContext();
 
   const [messages, setMessages] = useState([])
-
   const [loading, setLoading] = useState(false)
+
+  const [prompt, setPrompt] = useState('')
+  const [mode, setMode] = useState('text')
+  const [isPublished, setIsPublished] = useState(false)
+
+  const onSubmit = async (e)=>{
+    e.preventDefault()
+  }
 
   useEffect(() => {
     if (selectedChat) {
@@ -19,12 +28,21 @@ const ChatBox = () => {
     }
   }, [selectedChat]);
 
+  useEffect(() => {
+    if(containerRef.current){
+      containerRef.current.scrollTo({
+        top: containerRef.current.scrollHeight,
+        behaviour: "smooth"
+      })
+    }
+  }, [messages])
+  
 
   return (
     <div className='flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40'>
 
       {/* Chat Messages */}
-      <div className='flex-1 mb-5 overflow-y-scroll'>
+      <div ref={containerRef} className='flex-1 mb-5 overflow-y-scroll'>
         {messages.length === 0 && (
           <div className='h-full flex flex-col items-center justify-center gap-2 text-primary'>
             <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} alt="" className='w-full max-w-56 sm:max-w-68'/>
@@ -39,15 +57,32 @@ const ChatBox = () => {
         {/* Three Dots Loading */}
         {
           loading && <div className='loader flex items-center gap-1.5'>
-            <div className='w-1.5 h-1.5 rounded-full bg-gray-400 dark-bg-white animate-bounce'></div>
-            <div className='w-1.5 h-1.5 rounded-full bg-gray-400 dark-bg-white animate-bounce'></div>
-            <div className='w-1.5 h-1.5 rounded-full bg-gray-400 dark-bg-white animate-bounce'></div>
+            <div className='w-1.5 h-1.5 rounded-full bg-gray-400 dark:bg-white animate-bounce'></div>
+            <div className='w-1.5 h-1.5 rounded-full bg-gray-400 dark:bg-white animate-bounce'></div>
+            <div className='w-1.5 h-1.5 rounded-full bg-gray-400 dark:bg-white animate-bounce'></div>
           </div>
         }
       </div>
 
-      {/* Prompt Input Box */}
+      {mode === 'image' && (
+        <label className='inline-flex items-center gap-2 mb-3 text-sm mx-auto'>
+          <p className='text-sm'>Published Generated Image to Community</p>
+          <input type="checkbox" className='cursor-pointer' checked={isPublished} onChange={(e)=>setIsPublished(e.target.checked)}/>
+        </label>
+      )}
 
+      {/* Prompt Input Box */}
+      <form onSubmit={onSubmit}
+      className='bg-primary/20 dark:bg-[#583c79]/30 border border-primary dark:border-[#80609f]/30 rounded-full w-full max-w-2xl p-3 pl-4 mx-auto flex gap-4 items-center'>
+        <select onChange={(e)=>setMode(e.target.value)} value={mode} className='text-sm pl-2 pr-1 outline-none'>
+          <option className='dark:bg-purple-900' value="text">Text</option>
+          <option className='dark:bg-purple-900' value="image">Image</option>
+        </select>
+        <input onChange={(e)=>setPrompt(e.target.value)} value={prompt} type="text" placeholder='Type your prompt here...' className='flex-1 w-full text-md outline-none' required/>
+        <button disabled={loading}>
+          <img src={loading ? assets.stop_icon : assets.send_icon} className='w-7 cursor-pointer' alt="" />
+        </button>
+      </form>
       
     </div>
   )
